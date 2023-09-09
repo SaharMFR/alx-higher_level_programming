c6ccc1013405a892467e8b81d16242e39159a04e
SaharMFR <sweetysahora@gmail.com>
2023-09-09 10:00:57 -0700

    Task-8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence:
+        returns = len(sentence), sentence[0]
+    else:
+        returns = len(sentence), None
+    return returns
