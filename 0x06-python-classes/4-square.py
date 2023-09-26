697e79616550f6c73e885d8ff3a63b9eef7eb80f
SaharMFR <sweetysahora@gmail.com>
2023-09-26 12:41:51 -0700

    Task-4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0):
+        """Initializing a new Square.
+        Args:
+            size: The size of the new Square (int)."""
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the current square area."""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """To retrieve the private instance attribute 'size'."""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """To set the private instance attribute 'size'."""
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
