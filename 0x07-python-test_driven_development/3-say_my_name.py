fc6a76c77a1471b6703184a5ea39595739fd6b12
SaharMFR <sweetysahora@gmail.com>
2023-10-06 06:30:43 -0700

    documentation for task-2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+"""
+Defines a function to say:
+`My name is ` + first_name + ` ` + last_name
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Prints (`My name is ` + first_name + ` ` + last_name)
+
+    Args:
+        first_name: the first name.
+        last_name: the last name (empty string by default)
+
+    Raises:
+        TypeError: when any one of the two arguments is not string.
+    """
     if type(first_name) is not str:
         raise TypeError('first_name must be a string')
     elif type(last_name) is not str:
