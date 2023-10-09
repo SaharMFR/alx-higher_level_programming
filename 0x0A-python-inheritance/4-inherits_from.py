2eb140e1f914ef637d1e8ad082f6766bbc4aaad2
SaharMFR <sweetysahora@gmail.com>
2023-10-09 14:48:25 -0700

    Adding 2 lines after documentation in task-4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,8 @@ Defines a function to check if the object is an instance
 of a class that inherited (directly or indirectly) from the
 specified class.
 """
+
+
 def inherits_from(obj, a_class):
     """
     Returns `True` if the object is an instance of a class
