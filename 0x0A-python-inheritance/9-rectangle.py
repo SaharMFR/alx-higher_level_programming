45acd5aa61dcad8c9082c69d65631a8ff852cf0c
SaharMFR <sweetysahora@gmail.com>
2023-10-09 16:06:31 -0700

    Task-9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Defines a class Rectangle that inherits from BaseGeometry.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        desc = "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+        return desc
