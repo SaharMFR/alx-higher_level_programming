35b637675778ce4dd86dbf47b6093886f6630050
SaharMFR <sweetysahora@gmail.com>
2023-10-09 14:17:34 -0700

    update in task-1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,4 @@ class MyList(list):
     """Has some added features"""
     def print_sorted(self):
         """Prints the list sorted (ascending order)"""
-        sorted_list = self.copy()
-        sorted_list.sort()
-        print(sorted_list)
+        print(sorted(self))
