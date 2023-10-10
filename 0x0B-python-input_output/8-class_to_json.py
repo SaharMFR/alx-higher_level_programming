6a1cd1215b90c10ae7acc7ff811808cea1fd335f
SaharMFR <sweetysahora@gmail.com>
2023-10-10 16:24:18 -0700

    Task-8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Defines a function that returns the dictionary description with
+simple data structure for JSON serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """
+    Rreturns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization
+    of an object.
+    """
+    return obj.__dic__
