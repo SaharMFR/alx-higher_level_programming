a4a93a8dfee6f792a57ebd43b8bef69d5ae68a66
SaharMFR <sweetysahora@gmail.com>
2023-10-10 16:43:44 -0700

    Task-10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a class Student"""
+
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list and all(type(a) is str for a in attrs):
+            return {k: self.__dict__[k] for k in self.__dict__.keys() & attr}
+        return self.__dict__
