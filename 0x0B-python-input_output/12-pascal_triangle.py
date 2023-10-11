7aca539a704a5e58145970cd8c6a02e29e5a635a
SaharMFR <sweetysahora@gmail.com>
2023-10-10 17:32:40 -0700

    Task-12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a function creates the Pascal's triangle"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing
+    the Pascal's triangle of `n`.
+    """
+    p_triangle = []
+    if n > 0:
+        p_triangle.append([1])
+        for i in range(n):
+            last = p_triangle[-1]
+            temp = [1]
+            for j in range(len(last) - 1):
+                temp.append(last[j] + last[j + 1])
+            temp.append(1)
+            p_triangle.append(temp)
+
+    return p_triangle
