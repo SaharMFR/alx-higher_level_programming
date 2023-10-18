45bb59d794c6549dccfa8bd7a439ab6f0e6dcdb9
SaharMFR <sweetysahora@gmail.com>
2023-10-18 08:51:52 -0700

    Task-16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,14 @@ class Base:
         if list_dictionaries and list_dictionaries != []:
             return json.dumps(list_dictionaries)
         return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+        f_name = = cls.__name__ + '.json'
+        with open(f_name, 'w') as f:
+            if list_objs:
+                list_dictionaries = obj.to_dictionary() for obj in list_objs
+                f.write(Base.to_json_string(list_dictionaries))
+            else:
+                f.write("[]")
