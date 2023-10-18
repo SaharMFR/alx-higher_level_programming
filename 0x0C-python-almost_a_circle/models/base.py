33f1a1cdab7838296ffc3d640d8084ed75b25589
SaharMFR <sweetysahora@gmail.com>
2023-10-18 09:00:38 -0700

    documentation for the constructor in base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,6 +9,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initializes the new instace of the class (the constructor)"""
         if id is not None:
             self.id = id
         else:
