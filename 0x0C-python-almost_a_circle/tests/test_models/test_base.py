a090276ae1b02faa55bd9ff6b22b99e53d6b0f67
SaharMFR <sweetysahora@gmail.com>
2023-10-20 14:33:17 -0700

    update in task-0

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+import os
 
 """Test cases for `base` module"""
 
