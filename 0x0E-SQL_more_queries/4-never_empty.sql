28d3b5201ffd2b3acbd1549412b2da677dd98d27
SaharMFR <sweetysahora@gmail.com>
2023-11-15 15:23:08 -0800

    Task-4

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- Creates the table `id_not_null` with INT id with default value 1, and VARCHAR(256) name.
+CREATE TABLE IF NOT EXISTS id_not_null (
+	id INT NOT NULL DEFAULT 1,
+	name VARCHAR(256)
+	);
