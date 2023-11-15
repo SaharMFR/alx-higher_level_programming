10d96ab33395375125cd70b49b987ba21b43b69c
SaharMFR <sweetysahora@gmail.com>
2023-11-14 16:07:12 -0800

    Task-10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists all records of the table `second_table` of the database `hbtn_c0_0`.
+-- Diplay (score and name) descending ordered by score.
+SELECT score, name
+FROM second_table
+ORDER BY score DESC
