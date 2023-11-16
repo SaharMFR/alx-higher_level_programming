31bbe69984e317639857f14aaca28dc8b1af8470
SaharMFR <sweetysahora@gmail.com>
2023-11-16 14:57:41 -0800

    update3 in task-7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,5 +8,4 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_id) REFERNECES hbtn_0d_usa.states(id)
-	);
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
