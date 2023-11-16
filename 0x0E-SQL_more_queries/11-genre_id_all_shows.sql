2dfe30fce10a22fce9444b43e68d787c23f8b086
SaharMFR <sweetysahora@gmail.com>
2023-11-16 15:58:20 -0800

    Task-11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- Lists all shows contained in the database `hbtn_0d_tvshows`.
+-- Displaying `tv_shows.title` and `tv_show_genres.genre_id` ordered by `tv_shows.title` and `tv_show_genres.genre_id`.
+-- Displays NULL if a show doesn’t have a genre.
+-- Using one SELECT statement.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genre
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
+
