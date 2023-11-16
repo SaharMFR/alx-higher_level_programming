7217894480a5ea92c3c349f9127c1a761f726c4c
SaharMFR <sweetysahora@gmail.com>
2023-11-16 14:04:53 -0800

    Task-6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Creates the database `hbtn_0d_usa` and the table `states` with INT id and VARCHAR(256) name.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+	);
