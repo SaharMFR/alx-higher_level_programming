1cf7ee992fab4ade76f9d6e33cd5171b45adeb7e
SaharMFR <sweetysahora@gmail.com>
2023-11-16 14:54:45 -0800

    update2 in task7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,7 +5,7 @@
 -- Doesn't fail if the database or the table already exists.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	FOREIGN KEY(state_id) REFERNECES hbtn_0d_usa.states(id)
