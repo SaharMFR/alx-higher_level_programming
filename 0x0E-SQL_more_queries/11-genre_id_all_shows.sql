97f32b7cb0a0d1264fd8aa645db082c82ac20aef
SaharMFR <sweetysahora@gmail.com>
2023-11-16 16:05:22 -0800

    update1 in task-11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,6 +3,7 @@
 -- Displays NULL if a show doesn’t have a genre.
 -- Using one SELECT statement.
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genre
-ON tv_shows.id = tv_show_genres.show_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
