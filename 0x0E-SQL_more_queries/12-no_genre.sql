d93029ddd59a7348a82819c1548949432c901c05
SaharMFR <sweetysahora@gmail.com>
2023-11-16 16:20:40 -0800

    update in task-12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,7 +2,8 @@
 -- Displays `tv_shows.title` and `tv_show_genres.genre_id` ordered by `tv_shows.title` and `tv_show_genres.genre_id.
 -- Using one SELECT statement.
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_shows_genres.show_id IS NULL
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
