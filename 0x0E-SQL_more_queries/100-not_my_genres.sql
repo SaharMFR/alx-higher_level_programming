d25244a9ad0ff780b99a5ceca0d167ba2a93e622
SaharMFR <sweetysahora@gmail.com>
2023-11-16 17:03:56 -0800

    update in task-17

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,5 +7,5 @@ WHERE tv_genres.id NOT IN (SELECT tv_genres.id
 			   ON tv_genres.id = tv_show_genres.genre_id
 			   INNER JOIN tv_shows
 			   ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title = "Code")
+			   WHERE tv_shows.title = "Code")
 ORDER BY tv_genres.name;
