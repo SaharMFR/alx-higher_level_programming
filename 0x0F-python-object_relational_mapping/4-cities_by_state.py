ba4c1d0ad70b5b4108dee9363dffe6a02b4baea8
SaharMFR <sweetysahora@gmail.com>
2023-12-17 17:16:59 -0800

    fixing comma instead of dot in task-4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states,name \
+    cursor.execute("SELECT cities.id, cities.name, states.name \
                     FROM cities JOIN states ON states.id = cities.state_id")
     selected = cursor.fetchall()
 
