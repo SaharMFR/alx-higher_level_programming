cc79ce487ee65411ac319f1c9f304246ec4b8ef0
SaharMFR <sweetysahora@gmail.com>
2023-12-22 16:10:40 -0800

    Task-14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Prints all `City` objects from the database `hbtn_0e_6_usa`"""
+
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for obj in (session.query(State.name, City.id, City.name)
+                .filter(State.id == City.state_id)):
+        print(obj[0] + ": (" + str(obj[1]) + ") " + obj[2])
