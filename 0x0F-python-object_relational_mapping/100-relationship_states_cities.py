17188bf3c63e9422fe90b57285e2aa7089947851
SaharMFR <sweetysahora@gmail.com>
2023-12-22 17:12:45 -0800

    update2 in task-15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -14,12 +14,12 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_pi)
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    newState = State(name="California")
     newCity = City(name="San Francisco")
+    newState = State(name="California")
     newState.cities.append(newCity)
     session.add(newState)
     session.add(newCity)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -10,7 +10,7 @@ Base = declarative_base(metadata=mymetadata)
 
 
 class State(Base):
-    """Represents State with `id` and `name` attributes"""
+    """Represents State with `id`, `name` and `cities` attributes"""
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
