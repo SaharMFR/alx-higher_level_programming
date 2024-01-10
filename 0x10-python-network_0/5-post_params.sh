39140928f12f46d2b6cb5fe53f2bce150468df53
SaharMFR <sweetysahora@gmail.com>
2024-01-10 12:13:47 -0800

    update in task-5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a POST request to the passed URL, and displays the body of the response. A variable `email` must be sent with the value `test@gmail.com`, and a variable `subject` must be sent with the value `I will always be here PLD'.
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here PLD" "$1"
+# Takes in a URL, sends a POST request to the passed URL, and displays the body of the response. A variable `email` must be sent with the value `test@gmail.com`, and a variable `subject` must be sent with the value `I will always be here for PLD'.
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
