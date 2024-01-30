9e864500fbc367b689cffc3728fa8fe72a10edcb
SaharMFR <sweetysahora@gmail.com>
2024-01-30 11:35:43 -0800

    Task-8 0x14

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request.get(url, (err, response, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const chars = JSON.parse(data).characters;
+    charsInOrder(chars, 0);
+  }
+});
+
+const charsInOrder = function(chars, i) {
+  if (i === chars.length) return;
+  request(chars[i], (err, response, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(data).name);
+      charsInOrder(chars, ++i);
+    }
+  });
+};
