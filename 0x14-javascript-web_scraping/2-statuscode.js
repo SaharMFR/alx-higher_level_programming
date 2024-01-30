9770717d1126dbbd3b60aa212102c123f41f60e9
SaharMFR <sweetysahora@gmail.com>
2024-01-30 08:33:50 -0800

    Single quotes in task-2 0x14

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 const url = process.argv[2];
 request.get(url, (err, response) => {
   if (err) console.log(err);
-  else console.log("code:", response.statusCode);
+  else console.log('code:', response.statusCode);
 });
